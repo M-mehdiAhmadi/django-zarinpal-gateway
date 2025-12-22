b8c3295af27828536846586bcc98191c26004baf
mehdi ahmadi <ahmadi.mehd2@gmail.com>
2025-12-22 22:11:54 +0330

    payment request updated

--- django_zarinpal_gateway/urls.py
+++ django_zarinpal_gateway/urls.py
@@ -4,7 +4,7 @@ from django_zarinpal_gateway import views
 app_name = "zarinpal"
 
 urlpatterns = [
-    path("payment-request/<int:pk>/", views.BaseTransactionRequestView.as_view(), name="payment_request"),
+    path("payment-request/", views.BaseTransactionRequestView.as_view(), name="payment_request"),
     path("payment-verify/", views.BaseTransactionVerifyView.as_view(), name="payment_verify"),
 ]
 
