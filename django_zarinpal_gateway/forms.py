01963a1ac57a58eb932b90d0b9bde00134aedf44
mehdi ahmadi <ahmadi.mehd2@gmail.com>
2025-12-27 23:17:23 +0330

    now form label fields use gettext_lazy

--- django_zarinpal_gateway/forms.py
+++ django_zarinpal_gateway/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from django_zarinpal_gateway.models import Transaction
+from django.utils.translation import gettext_lazy as _
 
 class TransactionForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.fields["amount"].label = _("Amount to Pay")
+        self.fields["description"].label = _("Description")
+        self.fields["email"].label = _("Email")
+        self.fields["mobile"].label = _("Mobile")
     class Meta:
         model = Transaction
         fields = [
