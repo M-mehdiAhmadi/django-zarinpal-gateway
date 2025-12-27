51b6f421794f371c797c8f10710963535543e1fe
mehdi ahmadi <ahmadi.mehd2@gmail.com>
2025-12-27 23:18:03 +0330

    verbose_name of app now use gettext_lazy

--- django_zarinpal_gateway/apps.py
+++ django_zarinpal_gateway/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 class MultipayConfig(AppConfig):
     name = "django_zarinpal_gateway"
-    verbose_name = "payment (zarinpal Payment Gateway)"
+    verbose_name = _("payment (zarinpal)")
