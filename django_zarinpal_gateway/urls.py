12190a037895e072448cc0bf2d5acd1b1c58d4a3
mehdi ahmadi <ahmadi.mehd2@gmail.com>
2025-12-27 23:15:27 +0330

    payment_verify_queryset created

--- django_zarinpal_gateway/urls.py
+++ django_zarinpal_gateway/urls.py
@@ -6,5 +6,6 @@ app_name = "zarinpal"
 urlpatterns = [
     path("payment-request/", views.BaseTransactionRequestView.as_view(), name="payment_request"),
     path("payment-verify/<str:authority>/", views.BaseTransactionVerifyView.as_view(), name="payment_verify"),
+    path("payment-verify/", views.BaseTransactionVerifyView.as_view(), name="payment_verify_queryset"),
 ]
 
